18e461f9d0c8ac6e9e397235ff91f585eda5ed71
3000 Dusch, Philipp <philipp.dusch@hzd.hessen.de>
2024-09-25 15:29:29 +0200

    Move rowobjects to commons and merge them with contrete postal implementations Some renamings Further work on event handling

--- datafactoryutilities-commons/src/main/java/de/dfutil/dao/Dao.java
+++ datafactoryutilities-commons/src/main/java/de/dfutil/dao/Dao.java
@@ -3,6 +3,7 @@ package de.dfutil.dao;
 import java.util.List;
 import java.util.Optional;
 
+//FIXME add RepositoryFactory type parameter and providing method, for e.g. redistemplate or jpa entitymanager
 public interface Dao <T>{
 
         Optional<T> get(long id);
@@ -14,5 +15,7 @@ public interface Dao <T>{
         void update(T t, String[] params);
 
         void delete(T t);
+
+
 
 }
--- datafactoryutilities-commons/src/main/java/de/dfutil/entities/ValidityStatus.java
+++ datafactoryutilities-commons/src/main/java/de/dfutil/entities/ValidityStatus.java
@@ -1,15 +1,62 @@
 package de.dfutil.entities;
 
+import java.util.Objects;
+
 public class ValidityStatus {
     private String date;
     private String key;
     private ArchivingsStatus state;
     private String keyNew;
 
+    public String keyNew() {
+        return keyNew;
+    }
+
+    public void setKeyNew(String keyNew) {
+        this.keyNew = keyNew;
+    }
+
+    public ArchivingsStatus state() {
+        return state;
+    }
+
+    public void setState(ArchivingsStatus state) {
+        this.state = state;
+    }
+
+    public String key() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String date() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     public ValidityStatus(String date, String key, ArchivingsStatus state, String keyNew) {
         this.date = date;
         this.key = key;
         this.state = state;
         this.keyNew = keyNew;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ValidityStatus that = (ValidityStatus) o;
+        return Objects.equals(date, that.date) && Objects.equals(key, that.key) && state == that.state && Objects.equals(keyNew, that.keyNew);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, key, state, keyNew);
     }
 }

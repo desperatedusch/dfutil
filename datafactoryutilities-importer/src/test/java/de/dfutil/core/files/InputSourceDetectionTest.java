c4be6f3fb1c008d749684c981ea2ca6bf66660c7
3000 Dusch, Philipp <philipp.dusch@hzd.hessen.de>
2024-11-29 18:49:34 +0100

    Basic InputSourceDetection

---
+++ datafactoryutilities-importer/src/test/java/de/dfutil/core/files/InputSourceDetectionTest.java
@@ -0,0 +1,40 @@
+package de.dfutil.core.files;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@Import(TestConfig.class)
+@SpringBootTest
+@ActiveProfiles({"dev"})
+@TestPropertySource(
+        properties =
+                """
+                            app.importer.inputsource.folders=D:/ideaWorkspaces/dfutil/datafactoryutilities-importer/src/test/resources/files2Import/Streetcode_Stand_2024-01;D:/ideaWorkspaces/dfutil/datafactoryutilities-importer/src/test/resources/files2Import/Streetcode_Stand_2024-10
+                            app.importer.inputsource.filenamemask=B*.DAT
+                        """)
+public class InputSourceDetectionTest {
+
+    @Autowired
+    private InputSourceDetection cut;
+
+    @Test
+    public void findInputFiles() {
+        List<Path> inputFiles = new ArrayList<>();
+        List<Path> inputSourceFolders = cut.inputSourceFolders();
+        for (Path path : inputSourceFolders) {
+            inputFiles.addAll(cut.findInputFiles(path));
+        }
+    }
+
+}

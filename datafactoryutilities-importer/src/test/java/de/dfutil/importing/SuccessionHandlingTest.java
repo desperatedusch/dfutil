8a0bea56bcb247d295c484ec1ac206f4c4d45e49
3000 Dusch, Philipp <philipp.dusch@hzd.hessen.de>
2025-03-03 16:10:36 +0100

    H2 usage in Test

--- datafactoryutilities-importer/src/test/java/de/dfutil/importing/SuccessionHandlingTest.java
+++ datafactoryutilities-importer/src/test/java/de/dfutil/importing/SuccessionHandlingTest.java
@@ -1,4 +1,11 @@
 package de.dfutil.importing;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class SuccessionHandlingTest {
+
 }

19856b4ecd29fae6ca0d265aded66ea9e64a1a39
3000 Dusch, Philipp <philipp.dusch@hzd.hessen.de>
2025-06-11 16:01:02 +0200

    Replacements and Orphanes finally work

--- scripts/elk/startElk.bat
+++ scripts/elk/startElk.bat
@@ -1,4 +1,4 @@
-@ECHO OFF
-
-SET JAVA_HOME=$JDKPath$
+SET JAVA_HOME=D:\tools\jdks\temurin-24.0.1
 START D:\tools\elk\elasticsearch-9.0.2\bin\elasticsearch.bat
+START D:\tools\elk\kibana-9.0.1\bin\kibana.bat
+start D:\tools\elk\logstash-9.0.1\bin\logstash.bat
